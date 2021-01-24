a8e82fa23705f787e0d293fccdf8e8a513eac68d
Sanjay Patel <worldprotv@gmail.com>
2021-01-24 20:20:10 +0530

    basic vuejs file

---
+++ billion-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Home />
+  </v-app>
+</template>
+
+<script>
+import Home from "./components/Home";
+export default {
+  name: "App",
+
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
